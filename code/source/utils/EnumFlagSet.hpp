186f13a29e07057b7d7174ad50f5c81b93513253
MrAviator93 <kalninsarturs@hotmail.com>
2024-06-04 22:43:08 +0100

    Add additional methods & documentation for EnumFlagSet

--- code/source/utils/EnumFlagSet.hpp
+++ code/source/utils/EnumFlagSet.hpp
@@ -16,16 +16,20 @@ public:
 	using IntType = I;
 	using EnumType = E;
 
+	/// Default constructor.
 	constexpr EnumFlagSet() noexcept = default;
 
+	/// Constructor from an integral value.
 	explicit constexpr EnumFlagSet( IntType v ) noexcept
 		: m_value{ v }
 	{ }
 
+	/// Constructor from an enum value.
 	explicit constexpr EnumFlagSet( EnumType e ) noexcept
 		: m_value{ static_cast< IntType >( e ) }
 	{ }
 
+	/// Variadic constructor from multiple bits, accepts IntType and or EnumType.
 	template < typename... Bits >
 		requires( ( std::is_same_v< Bits, IntType > || std::is_same_v< Bits, EnumType > ) && ... )
 	constexpr EnumFlagSet( Bits... bits ) noexcept
@@ -37,23 +41,66 @@ public:
 	EnumFlagSet& operator=( const EnumFlagSet& ) = default;
 	EnumFlagSet& operator=( EnumFlagSet&& ) = default;
 
+	/// Resets all flags.
 	constexpr void reset() noexcept { m_value = {}; }
 
+	/// Sets a specific bit.
 	constexpr void set( EnumType bit ) noexcept { m_value |= static_cast< IntType >( bit ); }
 
+	///  Variadic set function to set multiple bits.
+	template < typename... Bits >
+		requires( sizeof...( Bits ) > 0 && ( std::is_same_v< Bits, IntType > || std::is_same_v< Bits, EnumType > ) &&
+				  ... )
+	constexpr void set( Bits... bits ) noexcept
+	{
+		( m_value |= ( std::is_same_v< Bits, EnumType > ? static_cast< IntType >( bits ) : bits ), ... );
+	}
+
+	/// Clears a specific bit.
 	constexpr void clear( EnumType bit ) noexcept { m_value &= ~static_cast< IntType >( bit ); }
 
+	///  Variadic clear function to clear multiple bits.
+	template < typename... Bits >
+		requires( sizeof...( Bits ) > 0 && ( std::is_same_v< Bits, IntType > || std::is_same_v< Bits, EnumType > ) &&
+				  ... )
+	constexpr void clear( Bits... bits ) noexcept
+	{
+		( m_value &= ~( std::is_same_v< Bits, EnumType > ? static_cast< IntType >( bits ) : bits ), ... );
+	}
+
+	/// Toggles a specific bit.
 	constexpr void toggle( EnumType bit ) noexcept { m_value ^= static_cast< IntType >( bit ); }
 
+	///  Variadic toggle function to toggle multiple bits.
+	template < typename... Bits >
+		requires( sizeof...( Bits ) > 0( std::is_same_v< Bits, IntType > || std::is_same_v< Bits, EnumType > ) && ... )
+	constexpr void clear( Bits... bits ) noexcept
+	{
+		( m_value &= ~( std::is_same_v< Bits, EnumType > ? static_cast< IntType >( bits ) : bits ), ... );
+	}
+
+	/// Tests if a specific bit is set.
 	[[nodiscard]] constexpr bool test( EnumType bit ) const noexcept
 	{
 		return ( m_value & static_cast< IntType >( bit ) ) != 0;
 	}
 
+	///  Variadic test function to test multiple bits.
+	template < typename... Bits >
+		requires( sizeof...( Bits ) > 0 && ( std::is_same_v< Bits, IntType > || std::is_same_v< Bits, EnumType > ) &&
+				  ... )
+	constexpr void test( Bits... bits ) noexcept
+	{
+		return ( ( m_value & ( std::is_same_v< Bits, EnumType > ? static_cast< IntType >( bits ) : bits ) ) && ... );
+	}
+
+	/// Checks if any bit is set.
 	[[nodiscard]] constexpr bool any() const noexcept { return m_value != 0; }
 
+	/// Checks if no bits are set.
 	[[nodiscard]] constexpr bool none() const noexcept { return m_value == 0; }
 
+	/// Counts the number of set bits.
 	[[nodiscard]] constexpr std::size_t count() const noexcept
 	{
 		std::size_t count{};
