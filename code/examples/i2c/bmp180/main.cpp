9fc253ba3ac7a7c1664583bdc4bbd24b6965ab61
MrAviator93 <kalninsarturs@hotmail.com>
2024-10-29 22:57:01 +0000

    Implement bmp180 example

--- code/examples/i2c/bmp180/main.cpp
+++ code/examples/i2c/bmp180/main.cpp
@@ -1,5 +1,92 @@
 
-int main()
+// Include I2C library files
+#include <utils/Timer.hpp>
+#include <i2c/Controllers.hpp>
+
+// Output
+#include <print>
+#include <algorithm>
+
+int main( const int argc, const char* const* const argv )
 {
+	const std::vector< std::string_view > args( argv, std::next( argv, static_cast< std::ptrdiff_t >( argc ) ) );
+
+	// Default name of i2c bus on RPI 4
+	std::string deviceName{ "/dev/i2c-1" };
+
+	if( args.size() >= 2 )
+	{
+		deviceName = args[ 1 ];
+	}
+
+	// Create a bus controller for the I2C bus
+	pbl::i2c::BusController busController{ deviceName };
+
+	// Check if the I2C bus is open and accessible
+	if( !busController.isOpen() )
+	{
+		std::println( stderr, "Failed to open I2C device" );
+		return 1;
+	}
+
+	// Create an BMP180 controller, attached to the bus controller
+	pbl::i2c::BMP180Controller bmp180{
+		busController, pbl::i2c::BMP180Controller::DEFAULT, pbl::i2c::BMP180Controller::ULTRA_HIGH_RESOLUTION };
+	pbl::utils::Timer timer{ std::chrono::milliseconds( 500 ) };
+
+	while( true )
+	{
+		if( timer.hasElapsed() )
+		{
+			std::println("----------------------------------------------------------");
+			const auto checkAndPrintError = [] (const auto& reading) {
+				if (!reading)
+				{
+					std::println( stderr, "{}", pbl::utils::toStringView( reading.error() ) );
+					return false;
+				}
+
+				return true;
+			};
+
+			const auto temp = bmp180.getTrueTemperatureC();
+			if( temp.has_value() )
+			{
+				std::println( "True temp: {} C", temp.value() );
+			}
+
+			const auto truePress = bmp180.getTruePressurePa();
+			if( truePress.has_value() )
+			{
+				std::println( "True pressure: {} Pa", truePress.value() );
+			}
+
+			const auto alt = bmp180.getAbsoluteAltitude();
+			if( alt.has_value() )
+			{
+				std::println( "Absolute altitude: {} m", alt.value() );
+			}
+
+
+			if (!checkAndPrintError(temp)) 
+			{
+				
+			}
+
+			if (!checkAndPrintError(truePress))
+			{
+
+			}
+
+			if (!checkAndPrintError(alt))
+			{
+
+			}
+
+			// Reset the timer
+			timer.set();
+		}
+	}
+
 	return 0;
 }
