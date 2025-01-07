d9e93db69382bf909aaab0a0f00ebf64fe2bc23b
Artur <kalninsarturs@hotmail.com>
2025-01-07 22:44:03 +0000

    Forgot to add namespace for Result alias

--- code/source/utils/Result.hpp
+++ code/source/utils/Result.hpp
@@ -6,7 +6,11 @@
 // C++
 #include <expected>
 
+namespace pbl::utils
+{
+
 template < typename T >
 using Result = std::expected< T, Error >;
 
+} // namespace pbl::utils
 #endif // PBL_UTILS_RESULT_HPP__
