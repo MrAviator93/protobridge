bfa78136a4c1d5607ed72837b316a3f261d52f36
MrAviator93 <kalninsarturs@hotmail.com>
2025-05-29 20:04:24 +0100

    Adjust cmake for more tests

---
+++ code/test/i2c/BusControllerMock.hpp
@@ -0,0 +1,8 @@
+#ifndef PBL_TEST_I2C_BUS_CONTROLLER_MOCK_HPP__
+#define PBL_TEST_I2C_BUS_CONTROLLER_MOCK_HPP__
+
+namespace pbl::i2c
+{
+
+} // namespace pbl::i2c
+#endif // PBL_TEST_I2C_BUS_CONTROLLER_MOCK_HPP__
